b1e3de471dfae8dc6aaff5b14eda1497e189ef39
Héctor Morales Palma <rehoscript@gmail.com>
2014-09-09 09:45:32 -0500

    first commit

---
+++ view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1><?=$confidential?></h1>
+        <hr>
+        <h2><?=$language?></h2>
+    </body>
+</html>
